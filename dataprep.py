0938e341343ed4d901c5e8269d66dfaef51ee165
leonardo.j.silva <leonardo.j.silva@inesctec.pt>
2020-12-31 00:01:59 +0000

    Alteração do preparamento dos dados

--- dataprep.py
+++ dataprep.py
@@ -11,12 +11,12 @@ class TP2EAC(object):
         self.status_encoder = pickle.load(open('preparation/status_civic_encoder.pkl','rb'))
 
     def data_preparation(self, df):
-        df['age'] = self.age.transform(df[['age']].values)
-        df['capital_gain'] = self.capital_gain.transform(df[['capital_gain']].values)
-        df['capital_loss'] = self.capital_loss.transform(df[['capital_loss']].values)
-        df['education'] = self.education.transform( df[['education']].values )
-        df['hours_per_week'] = self.hours.transform(df[['hours_per_week']].values)
-        df['status_civic'] = self.status.transform(df[['status_civic']].values)
+        df['age'] = self.age.transform(df[['age']])
+        df['capital_gain'] = self.capital_gain.transform(df[['capital_gain']])
+        df['capital_loss'] = self.capital_loss.transform(df[['capital_loss']])
+        df['education'] = self.education.transform( df[['education']] )
+        df['hours_per_week'] = self.hours.transform(df[['hours_per_week']])
+        df['status_civic'] = self.status.transform(df[['status_civic']])
 
         return df
 
